0015a7715c8ad37ade45cbffdbab68c985bb7dc9
Tomi-gomez <tomasngomez1@gmail.com>
2025-07-25 16:02:18 -0300

    Feat: Agrego Docker y Dockerfile

--- msvc-usuarios/src/main/java/org/aguzman/springcloud/msvc/usuarios/client/CursoClientRest.java
+++ msvc-usuarios/src/main/java/org/aguzman/springcloud/msvc/usuarios/client/CursoClientRest.java
@@ -4,9 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-cursos",url = "localhost:8002")
+@FeignClient(name = "msvc-cursos",url = "host.docker.internal:8002")
 public interface CursoClientRest {
 
-    @DeleteMapping("/eliminar-usuario/{id}")
+    @DeleteMapping("/eliminar-usuario-curso/{id}")
     void eliminarCursoUsuarioPorId(@PathVariable Long id);
 }
